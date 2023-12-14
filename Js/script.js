bd09293c46690c5c8906dfb9250504407b4b459d
Priscilla Miranda <priscilla-rose@hotmail.com>
2023-12-13 22:46:48 -0300

    commit de continuação

--- Js/script.js
+++ Js/script.js
@@ -1,27 +1,49 @@
-function imc(){
+const firstDiv = document.querySelector('.first-step');
+const secondDiv = document.querySelector('.second-step');
+const finalDiv = document.querySelector('.final-step');
+    
+    function go(currentStep,nextStep)
+        {
+            let dNone, dBlock;
+            if(currentStep == 1)
+            {
+                dNone = firstDiv;
+            }
+            else if(currentStep == 2)
+            {
+                dNone = secondDiv;
+            }
+            else
+            {
+                dNone = finalDiv;
+            }
+            dNone.style.display = 'none';
 
-let usuario = window.prompt("Digite seu nome: ");
-let peso = Number(window.prompt("Digite seu peso:").replace(",","."));
-let altura = Number(window.prompt("Digite sua altura:").replace(",","."));
+            if(nextStep == 1)
+            {
+                dBlock = firstDiv;
+            }
+            else if(nextStep == 2)
+            {
+                dBlock = secondDiv;
+            }
+            else
+            {
+                dBlock = finalDiv;
+            }
+            dBlock.style.display = 'block';
+        }
+       
+    
+function validateImc(){
+   const peso = document.getElementById("peso");
+   const altura = document.getElementById("altura");
 
-dados = ("<h3>Nome:</h3>" + usuario + "<br>"  + "<h3>Peso:</h3>" + peso + "kg" + "<br>" + "<h3>Altura:</h3>" + altura + "cm" + "<br>");
-
-document.write("<h1>Resultado:</h1>" + dados + "<br>");
-
-let imc = Number(peso/(altura*altura));
- document.write("<h1>Seu IMC:<h1>" + imc + "<br>");
-
- if(imc < 18.5){
-    document.write("<h3> Classificação de IMC: <h3>" + "Magro");
- } else if (imc < 25){
-    document.write("<h3>Classificação de IMC:<h3>" + "Normal");
- } else if (imc < 30){
-    document.write("<h3>Classificação de IMC:<h3>" + "Sobrepeso");
- } else if (imc < 35){
-    document.write("<h3>Classificação de IMC:<h3>" + "Obesidade grau I");
- } else if (imc < 40){
-    document.write("<h3>Classificação de IMC:<h3>" + "Obesidade grau II");
- } else if(imc > 40){
-    document.write("<h3>Classificação de IMC:<h3>" + "Obesidade grau III");
- }
+   if(!peso.value || !altura.value)
+   {
+      console.log("campos incorretos");
+   }
+   else{
+      console.log("tudo ok! Calcular")
+   }
 }
